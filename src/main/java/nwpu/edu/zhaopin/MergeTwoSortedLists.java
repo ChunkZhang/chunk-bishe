c7aa85fb409365d3a02f244043f22df96037d29f
chunk <344900109@qq.com>
2017-08-21 09:43:16 +0800

    20170821

---
+++ src/main/java/nwpu/edu/zhaopin/MergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+package nwpu.edu.zhaopin;
+
+/**
+ * Created by chunk on 2017/8/18.
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1==null){
+            return l2;
+        }
+        if (l2==null){
+            return l1;
+        }
+        ListNode listNode ;
+        if (l1.val < l2.val){
+            listNode = l1;
+            listNode.next = mergeTwoLists(l1.next,l2);
+        }else {
+            listNode = l2;
+            listNode.next = mergeTwoLists(l1, l2.next);
+        }
+        return listNode;
+    }
+}
+
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
