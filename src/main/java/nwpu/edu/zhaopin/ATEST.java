f5d9ccf9d835129160c8ff44758e4a1e45e20ca9
chunk <344900109@qq.com>
2017-09-05 20:00:24 +0800

    1

--- src/main/java/nwpu/edu/zhaopin/ATEST.java
+++ src/main/java/nwpu/edu/zhaopin/ATEST.java
@@ -5,7 +5,7 @@ package nwpu.edu.zhaopin;
  */
 public class ATEST {
     public static void main(String[] args){
-        TrappingRainWater s = new TrappingRainWater();
-        s.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1});
+        RotateImage s = new RotateImage();
+        s.rotate(new int[][]{{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}});
     }
 }
---
+++ src/main/java/nwpu/edu/zhaopin/JumpGame.java
@@ -0,0 +1,84 @@
+package nwpu.edu.zhaopin;
+
+import java.util.List;
+
+/**
+ * Created by chunk on 2017/9/5.
+ */
+public class JumpGame {
+    public int jump(int[] nums) {
+//        return jump1(nums);
+//        return jump2(nums);
+        int reach = 0;
+        int count = 0;
+        int lastReach = 0;
+        for (int i = 0;i<reach&&i<nums.length;i++){
+            if (i>lastReach){
+                count++;
+                lastReach =reach;
+            }
+            reach = Math.max(reach,nums[i]+i);
+        }
+        if (reach<nums.length-1){
+            return 0;
+        }
+        return count;
+    }
+
+    private int jump3(int[] A){
+        if(A==null || A.length==0)
+            return 0;
+        int lastReach = 0;
+        int reach = 0;
+        int step = 0;
+        for(int i=0;i<=reach&&i<A.length;i++)
+        {
+            if(i>lastReach)
+            {
+                step++;
+                lastReach = reach;
+            }
+            reach = Math.max(reach,A[i]+i);
+        }
+        if(reach<A.length-1)
+            return 0;
+        return step;
+    }
+
+    private int jump2(int[] nums) {
+        if (nums.length==0){
+            return 0;
+        }
+        int[] count = new int[nums.length];
+        for (int i = nums.length-2;i>=0;i--){
+            count[i] = Integer.MAX_VALUE/2;
+            if (nums[i]==0) continue;
+            for (int j = i+1;j<nums.length&&j<=nums[i]+i;j++){
+                count[i] = Math.min(count[i],count[j]+1);
+            }
+        }
+
+        return count[0];
+    }
+
+    private int jump1(int[] nums) {
+        int[] num = new int[]{Integer.MAX_VALUE};
+        diguiJump(num,0,nums,0);
+        return num[0];
+    }
+
+    public void diguiJump(int num[], int count ,int[] nums , int index){
+        if (index>nums.length || count>num[0]){
+            return;
+        }
+        if (index == nums.length-1){
+            if (num[0]>count){
+                num[0] = count;
+            }
+        }else if (index < nums.length){
+            for (int i = index+1;i<=index+nums[index];i++){
+                diguiJump(num,count+1,nums,i);
+            }
+        }
+    }
+}
